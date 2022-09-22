067cfb8b409ed980c8be833ef83c57c100c7d26a
JasonJayPower <jasonjaypower@gmail.com>
2022-09-22 14:59:38 +0100

    Add GLAD as a submodule (OpenGL 3.3 core)

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -1,13 +1,21 @@
+#include <GLAD/glad.h>
 #include <GLFW/glfw3.h>
-
 
 int main(int argc, const char* argv[]) {
 
 	glfwInit();
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
 
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
 	while (!glfwWindowShouldClose(window)) {
+		glClearColor(0.39f, 0.58f, 0.93f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
